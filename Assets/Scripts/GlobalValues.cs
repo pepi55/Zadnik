7430c6c885992ffb014fc0803a617c879759ca29
Pepi Dimi <pepi55@live.com>
2014-02-06 14:05:07 +0100

    code cleanup

--- Assets/Scripts/GlobalValues.cs
+++ Assets/Scripts/GlobalValues.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public static class GlobalValues{
 	//strings
 	public static string cellName = "hexGridCell";
+
+	//floats
 	public static float SFXvol = 1;
 	public static float MAINvol = 1;
+
+	//ints
 	public static int KillCount = 909;
 	public static int ExpCount;
 	public static int GoldCount;
-
-	//vector2s
-	//array of positions of gridcells
 }
