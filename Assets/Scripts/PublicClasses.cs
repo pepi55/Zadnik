f7211057753be310cb0c51ca712c91e8d61a3da6
Pepi Dimi <pepi55@live.com>
2014-02-06 14:19:18 +0100

    public classes script added

---
+++ Assets/Scripts/PublicClasses.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PublicClasses : MonoBehaviour {
+	/*--- STRUCTS ---*/
+	public struct Point {
+		public int X, Y;
+
+		public Point (int x, int y) {
+			X = x;
+			Y = y;
+		}
+	}
+	/*--- END STRUCTS ---*/
+
+	/*--- CLASSES ---*/
+	public abstract class GridObject {
+		public Point Location;
+
+		public int X {
+			get {
+				return Location.X;
+			}
+		}
+
+		public int Y {
+			get {
+				return Location.Y;
+			}
+		}
+
+		public GridObject (Point location) {
+			Location = location;
+		}
+
+		public GridObject (int x, int y) : this(new Point(x, y)) {}
+
+		public override string ToString () {
+			return string.Format ("({0}, {1})", X, Y);
+		}
+	}
+	/*--- END CLASSES ---*/
+}
