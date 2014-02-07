b6577ebb35a694710e25d08b0145b61b82eba07d
Pepi Dimi <pepi55@live.com>
2014-02-07 11:34:01 +0100

    better grid generation

---
+++ Assets/Scripts/Grid/HexGridArray.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGridArray : MonoBehaviour {
+	/*--- PUBLICS ---*/
+	//transform
+	public Transform hex;
+
+	//int
+	public int gridHeight = 16;
+	public int gridLength = 16;
+
+	//float
+	public float rad = 0.5f;
+
+	//bool
+	public bool innerCircleRad = true;
+	/*--- END PUBLICS ---*/
+
+	/*--- PRIVATES ---*/
+	//float
+	private float offsetX, offsetY;
+
+	//array
+	private ArrayList hexes = new ArrayList();
+	/*--- END PRIVATES ---*/
+
+	// Use this for initialization
+	void Start () {
+		float unitLength = (innerCircleRad) ? (rad / (Mathf.Sqrt (3) / 2)) : rad;
+
+		offsetX = unitLength * Mathf.Sqrt (3);
+		offsetY = unitLength * 1.5f;
+
+		for (int x = 0; x < gridLength; x++) {
+			for (int y = 0; y < gridHeight; y++) {
+				Vector2 hexPos = HexOffset(x, y);
+				Instantiate(hex, hexPos, Quaternion.identity);
+			}
+		}
+	}
+
+	Vector2 HexOffset (int x, int y) {
+		Vector2 pos = Vector2.zero;
+
+		if (y % 2 == 0) {
+			pos.x = x * offsetX;
+			pos.y = y * offsetY;
+		} else {
+			pos.x = (x + 0.5f) * offsetX;
+			pos.y = y * offsetY;
+		}
+
+		return pos;
+	}
+}
