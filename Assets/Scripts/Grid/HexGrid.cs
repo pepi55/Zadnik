4ed6efe57fe6d4ed44c8375ec1168026bd75c09a
Pepi Dimi <pepi55@live.com>
2014-02-12 00:36:04 +0100

    grid array added

--- Assets/Scripts/Grid/HexGrid.cs
+++ Assets/Scripts/Grid/HexGrid.cs
@@ -22,8 +22,9 @@ public class HexGrid : MonoBehaviour {
 	//float
 	private float offsetX, offsetY;
 
-	//array
-	//>>> total amount of tiles <<<//
+	//list
+	private List<List<Vector3>> r = new List<List<Vector3>>();
+	private List<Vector3> q = new List<Vector3>();
 
 	//class
 	private class HexTile {
@@ -35,37 +36,68 @@ public class HexGrid : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		DrawGrid();
+
+		Debug.Log(r[0][9]);
 	}
 
 	private void DrawGrid () {
-		float unitLength = (innerCircleRad) ? (rad / (Mathf.Sqrt (3) / 2)) : rad;
+		float unitLength = (innerCircleRad) ? (rad / (Mathf.Sqrt(3) / 2)) : rad;
 		
-		offsetX = unitLength * Mathf.Sqrt (3);
+		offsetX = unitLength * Mathf.Sqrt(3);
 		offsetY = unitLength * 1.5f;
 		
 		for (int x = 0; x < gridLength; x++) {
 			for (int y = 0; y < gridHeight; y++) {
 				HexTile hexCell = new HexTile();
+				Vector3 column;
 				
 				hexCell.position = HexOffset(x, y);
 				hexCell.tile = (GameObject)Instantiate(hex, hexCell.position, Quaternion.identity);
+				hexCell.tile.transform.parent = transform;
 				hexCell.tile.name = GlobalValues.cellName;
 				hexCell.tile.tag = GlobalValues.cellTag;
+
+				column = AxisToCube(hexCell.position);
+				q.Add(column);
 			}
+
+			r.Add(q);
 		}
 	}
 
 	private Vector2 HexOffset (int x, int y) {
-		Vector2 pos = Vector2.zero;
+		Vector2 pos = new Vector2();
 
-		if (y % 2 == 0) {
-			pos.x = x * offsetX;
-			pos.y = y * offsetY;
-		} else {
-			pos.x = (x + 0.5f) * offsetX;
-			pos.y = y * offsetY;
+		float xp = y * offsetX;
+
+		if (x % 2 != 0) {
+			xp += offsetX / 2;
 		}
 
+		float yp = x * offsetY;
+
+		pos.x = xp;
+		pos.y = yp;
+
 		return pos;
+	}
+
+	private Vector2 CubeToAxis (Vector3 cube) {
+		float q, r;
+
+		q = cube.x;
+		r = cube.z;
+
+		return new Vector2 (q, r);
+	}
+
+	private Vector3 AxisToCube (Vector2 axis) {
+		float x, y, z;
+
+		x = axis.x;
+		z = axis.y;
+		y = -x - z;
+
+		return new Vector3(x, y, z);
 	}
 }
