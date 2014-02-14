8ba44453c4ebc1ca13af286f86a626c4c6d6f952
Pepi Dimi <pepi55@live.com>
2014-02-14 02:10:36 +0100

    Pathfind solution?

--- Assets/Scripts/AStar/IPathNode.cs
+++ Assets/Scripts/AStar/IPathNode.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public interface IPathNode<T> {
-	List<T> Connections { get; }
-	Vector3 Position { get; }
-	bool Invalid { get; }
+	List<T> connections { get; }
+	Vector2 position { get; }
+	bool invalid { get; }
 }
