fc99bdc9a082127b6485835d980e8762f4281293
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-02-17 14:41:10 +0100

    Sprites Complete

---
+++ Assets/Scripts/Animation/DummyAnimation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyAnimation : MonoBehaviour {
+	public bool Alive;
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Alive){
+			animator.SetBool("Alive",true);
+		}else{
+			animator.SetBool("Alive",false);
+		}
+		if(Input.GetKey(KeyCode.Space)){
+			if(Alive){
+				Debug.Log("hoi");
+				Alive = false;
+			}else{
+				Alive = true;
+			}
+		}
+	}
+}
