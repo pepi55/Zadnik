3ac1e06290e30e5c824c4ea6569df92b7088c4cd
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-02-18 16:38:46 +0100

    Dummy Bugless

--- Assets/Scripts/Animation/DummyAnimation.cs
+++ Assets/Scripts/Animation/DummyAnimation.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class DummyAnimation : MonoBehaviour {
 	private float radius;
-	public bool Alive;
 	private Animator animator;
+	private int HitAnim = 0;
+	private int HitPoints = 5;
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
@@ -14,27 +15,23 @@ public class DummyAnimation : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-		if(Alive){
-			animator.SetBool("Alive",true);
-		}else{
+		animator.SetInteger("Lives",HitPoints);
+		if(HitPoints == 0){
 			animator.SetBool("Alive",false);
 		}
-		if(Input.GetMouseButtonDown(0)){
+		if(HitAnim != 0){
+			Debug.Log("SEEEEEE");
+			HitAnim -= 1;
+			animator.SetInteger("Hit",HitAnim);
+		}
+		if(Input.GetMouseButtonDown(0) && HitPoints != 0){
 			Vector3 MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			float dist = Mathf.Pow(MousePos.x - transform.position.x,2) + Mathf.Pow(MousePos.y - transform.position.y,2);
 			dist = Mathf.Sqrt(dist);
-			if(dist > radius){
-				Debug.Log("hitting the dummy");
-				animator.SetTrigger("Hit");
-			}
-		}
-	
-		if(Input.GetKey(KeyCode.Space)){
-			if(Alive){
-				Alive = false;
-			}else{
-				Alive = true;
+			if(dist < radius){
+				HitPoints -= 1;
+				HitAnim = 50;
+				animator.SetInteger("Hit",HitAnim);
 			}
 		}
 	}
