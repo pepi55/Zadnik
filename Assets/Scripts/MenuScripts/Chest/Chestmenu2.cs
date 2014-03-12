76bd0b0dd0cc26c537820543192fbfcbbd52cd18
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-03-12 14:20:33 +0100

    Weapons added

--- Assets/Scripts/MenuScripts/Chest/Chestmenu2.cs
+++ Assets/Scripts/MenuScripts/Chest/Chestmenu2.cs
@@ -4,16 +4,15 @@ using System.Collections;
 public class Chestmenu2 : MonoBehaviour {
 	//	private List<Texture2D> ItemRandom = new List <Texture2D>();
 	
-	public Texture2D Item1, Item2, Item3,Item4, Item5, Item6;
-	public Texture2D ItemRandom1,ItemRandom2,ItemRandom3;
+	public Texture2D Item1, Item2, Item3;
 	
 	private float radius; 
 	private float Luck;
 	private float RandomNumber;
 	
 	public bool PopBool = false;
+	private bool Obtain1, Obtain2, Obtain3;
 	public bool doWindow0 = true;
-	private bool ChestReady = false;
 	
 	private Rect PopMenu = new Rect(Screen.width - 200 ,0,200,100);
 	
@@ -27,17 +26,30 @@ public class Chestmenu2 : MonoBehaviour {
 			float dist = Mathf.Pow(MousePos.x - transform.position.x,2) + Mathf.Pow(MousePos.y - transform.position.y,2);
 			dist = Mathf.Sqrt(dist);
 			if(dist < radius){
-				Debug.Log("WOOT");
+				Debug.Log(Obtain1);
 				PopBool = true;
 			}
 		}
 	}
 	void DoWindow0(int windowID ) {
-		if(GUI.Button(new Rect(0,30, 50,50),ItemRandom1,GUIStyle.none)){
-			Debug.Log("FUCK ME");
+		if (GUI.Button(new Rect(0,30, 50,50),	Item1,GUIStyle.none)){
+			if(!Obtain1){
+
+			}
+			Debug.Log("This slot is empty");
 		}
-		GUI.Button(new Rect(50,30, 50,50), ItemRandom2,GUIStyle.none);
-		GUI.Button(new Rect(100,30, 50,50), ItemRandom3,GUIStyle.none);
+		if (GUI.Button(new Rect(50,30, 50,50), 	Item2,GUIStyle.none)){
+			if(!Obtain2){
+				
+			}
+			Debug.Log("This slot is empty");
+		}
+		if (GUI.Button(new Rect(100,30, 50,50), Item3,GUIStyle.none)){
+			if(!Obtain3){
+				Debug.Log("OBTAINED SWORD");
+				Item3 = Item1;
+			}
+		}
 		if(GUI.Button(new Rect(10, 80, 100, 20), "Leave")){
 			Destroy(gameObject);
 			PopBool = false;
@@ -49,10 +61,8 @@ public class Chestmenu2 : MonoBehaviour {
 	}
 	void OnGUI() {
 		//doWindow0 = GUI.Toggle(new Rect(10, 10, 100, 20), doWindow0, "Window 0");
-		if(ChestReady){
-			if (PopBool){
-				GUI.Window(0, PopMenu, DoWindow0, "Chest",GUIStyle.none);
-			}
+		if (PopBool){
+			GUI.Window(0, PopMenu, DoWindow0, "Chest",GUIStyle.none);
 		}
 		
 	}
