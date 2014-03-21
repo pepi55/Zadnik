fc51cbe8c03462dae0fc7a3a2a8a42092cae5d91
Pepi Dimi <pepi55@live.com>
2014-03-21 18:42:56 +0100

    FRUSTRATING FFS

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -3,113 +3,147 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Movement : MonoBehaviour {
-	//float
-	//private float timer = 0;
-	//private float timer = 0;
+	//list
+	public static List<PathNode> sources;
+	public static List<PathNode> solvedPath = new List<PathNode>();
+
+	//gameobject
+	private GameObject start;
+	private GameObject end;
+
+	//int
+	private int startIndex;
+	private int endIndex;
+	private int lastStartIndex;
+	private int lastEndIndex;
+	private int place;
 
 	//bool
-	/*private bool reset;
-	private bool pathDone;*/
-	//private bool walking;
-	//private bool click = false;
-
-	//list
-	//public List<PathNode> solvedPath = new List<PathNode>();
-
-	//pathnode
-	//private PathNode hex;
-
-	//class
-	//private EnemyMove enemy;
+	private bool pathDone;
+	public static bool reset;
 
 	//ray
 	private RaycastHit2D selectHexRay;
 
+	//color
+	public Color nodeColor = new Color(0.05f, 0.3f, 0.05f, 0.1f);
+
 	void Start () {
-		//GlobalValues.playerPos = transform.position;
-		GlobalValues.player = /*this.*/gameObject;
+		sources = HexGrid.sources;
+		start = this.gameObject;
+		GlobalValues.player = this.gameObject;
 	}
 
 	void Update () {
-		if (/*!walking && */Input.GetMouseButtonDown(0) == true) {
+		if (Input.GetMouseButtonDown(0) == true) {
 			selectHexRay = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
 			if (selectHexRay.collider != null) {
-				StopCoroutine("MoveChar");
-				if (/* && */selectHexRay.collider.tag == GlobalValues.cellTag) {
-					HexGrid.end = selectHexRay.transform.gameObject;
+				StopCoroutine("Move");
 
-					StartCoroutine("MoveChar");
-				}/* else if (!walking) {
-					HexGrid.end = null;
-				}*/
+				if (selectHexRay.collider.tag == GlobalValues.cellTag) {
+					end = selectHexRay.transform.gameObject;
+
+					GetPath();
+					Debug.Log(solvedPath.Count);
+					//StartCoroutine("Move");
+				}
 			}
 		}
-		
-		//MoveChar();
-		/*if (Input.touchCount == 1) {
-			if (GlobalValues.active) {
-				RaycastHit2D selectHexRay = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position), Vector2.zero);
-				
-				if (selectHexRay.collider != null) {
-					Debug.Log("huehuehuehuehuehuehuehuehuehuehuehuehuehuehuehuehue");
-
-					if (Input.GetMouseButtonDown(0)) Debug.Log("0");
-					if (Input.GetMouseButtonDown(1)) Debug.Log("1");
-					if (Input.GetMouseButtonDown(3)) Debug.Log("2");
-				}
-				
-				if (selectHexRay.collider.tag == GlobalValues.cellTag) {
-					HexGrid.end = selectHexRay.transform.gameObject;
-					MoveChar();
-				}
-			}
-		}*/
 	}
 
-	private IEnumerator MoveChar () {
-		//int i = 0;
-		//walking = true;
-		EnemyMove enemyMovement;
-
-		while (true) {
-			//i++;
+	private IEnumerator Move () {
+		for (int i = 0; i < solvedPath.Count - 1; i++) {
+			transform.position = solvedPath[i].Position;
 			yield return new WaitForSeconds(0.1f);
-
-			HexGrid.solvedPath[0].tag = GlobalValues.cellTag;
-			transform.position = HexGrid.solvedPath[1].Position;
-			HexGrid.solvedPath[1].tag = GlobalValues.playerTag;
-
-			for (int i = 0; i < GlobalValues.enemies.Count; i++) {
-				yield return new WaitForSeconds(0.1f);
-				
-				enemyMovement = GlobalValues.enemies[i].GetComponent<EnemyMove>();
-				enemyMovement.solvedPath[0].tag = GlobalValues.cellTag;
-				
-				if (enemyMovement.solvedPath[1].tag == GlobalValues.cellTag) {
-					GlobalValues.enemies[i].transform.position = enemyMovement.solvedPath[1].Position;
-					enemyMovement.solvedPath[1].tag = GlobalValues.enemyTag;
-				}
-			}
-
-			if (HexGrid.solvedPath.Count == 2) {
-				//walking = false;
-				//GlobalValues.player = this.gameObject;
-
-				break;
-			}
-			
-			//yield return new WaitForSeconds(0.1f);
-			//GlobalValues.player = transform.position;
-			//GlobalValues.move = true;
-			/*Debug.Log(HexGrid.solvedPath[0]);
-			Debug.Log(HexGrid.solvedPath[0].tag);
-			HexGrid.solvedPath[0].tag = GlobalValues.cellTag;
-			
-			if (HexGrid.solvedPath[1].tag == GlobalValues.cellTag) {
-				transform.position = HexGrid.solvedPath[1].Position;
-				HexGrid.solvedPath[1].tag = GlobalValues.playerTag;
-			}*/
 		}
+	}
+
+	private void GetPath () {
+		while (true) {
+			if (reset) {
+				pathDone = false;
+				solvedPath.Clear();
+				reset = false;
+			}
+			
+			if (start == null) {
+				Debug.LogWarning("No start point");
+				//enabled = false;
+				
+				break; //return;
+			} else if (end == null) {
+				Debug.LogWarning("No end point");
+				//enabled = false;
+				
+				break; //return;
+			}
+			
+			startIndex = Closest(sources, start.transform.position);
+			endIndex = Closest(sources, end.transform.position);
+			
+			if (startIndex != lastStartIndex || endIndex != lastEndIndex) {
+				reset = true;
+				
+				lastStartIndex = startIndex;
+				lastEndIndex = endIndex;
+				
+				break; //return;
+			}
+			
+			for (int i = 0; i < sources.Count; i++) {
+				if (AStar.InvalidNode(sources[i])) continue;
+				sources[i].nodeColor = nodeColor;
+			}
+			
+			//PulsePoint(lastStartIndex);
+			//PulsePoint(lastEndIndex);
+			
+			if (!pathDone) {
+				/*for (int i = 0; i < 6; i++) {
+				solvedPath = AStar.CalculatePath(sources[lastStartIndex], sources[lastEndIndex]);
+			}*/
+				solvedPath = AStar.CalculatePath(sources[lastStartIndex], sources[lastEndIndex]);
+				/*for (int i = 0; i < solvedPath.Count; i++) {
+				Debug.Log(solvedPath[i]);
+			}*/
+				pathDone = true;
+			}
+			
+			if (solvedPath == null || solvedPath.Count < 1) {
+				Debug.LogWarning("Invalid Path");
+				reset = true;
+				enabled = false;
+				
+				break; //return;
+			}
+			
+			for (int i = 0; i < solvedPath.Count - 1; i++) {
+				if (AStar.InvalidNode(solvedPath[i]) || AStar.InvalidNode(solvedPath[i + 1])) {
+					reset = true;
+					break; //return;
+				}
+				
+				Debug.DrawLine(solvedPath[i].Position, solvedPath[i + 1].Position, Color.cyan * new Color(1.0f, 1.0f, 1.0f, 1.0f));
+			}
+		}
+	}
+
+	private static int Closest (List<PathNode> inNodes, Vector2 toPoint) {
+		int closestIndex = 0;
+		float minDistance = float.MaxValue;
+		
+		for (int i = 0; i < inNodes.Count; i++) {
+			if (AStar.InvalidNode(inNodes[i])) continue;
+			
+			float thisDistance = Vector2.Distance(toPoint, inNodes[i].Position);
+			
+			if (thisDistance > minDistance) continue;
+			
+			minDistance = thisDistance;
+			closestIndex = i;
+		}
+		
+		return closestIndex;
 	}
 }
