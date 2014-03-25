fd9a4a87591e3acd09bf8c579b578c73c5612928
Pepi Dimi <pepi55@live.com>
2014-03-25 09:26:10 +0100

    enemy rewrite

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerControl : MonoBehaviour {
+	/*--- PUBLICS ---*/
+	//list
+	public static List<PathNode> sources;
+	/*--- END PUBLICS ---*/
+
+	/*--- PRIVATES ---*/
+	//list
+	private List<PathNode> solvedPath = new List<PathNode>();
+	/*--- END PRIVATES ---*/
+
+	void Awake () {
+		sources = HexGrid.sources;
+	}
+
+	void OnEnable () {
+		//GameControler.OnClick +=
+	}
+
+	void OnDisable () {
+		//GameControler.OnClick -=
+	}
+}
