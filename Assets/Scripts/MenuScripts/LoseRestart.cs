630a5519b0db289943ac5e31ab23e2a9e2551346
Chanisco <chaniscotromp@chaniscos-mbp.media.local>
2014-04-08 11:44:12 +0200

    Fade in Complete <3

--- Assets/Scripts/MenuScripts/LoseRestart.cs
+++ Assets/Scripts/MenuScripts/LoseRestart.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LoseRestart : MonoBehaviour {
 
 	void OnMouseDown () {
+		Screen.orientation = ScreenOrientation.Portrait;
 		Application.LoadLevel("StartMenu");
 	}
 }
